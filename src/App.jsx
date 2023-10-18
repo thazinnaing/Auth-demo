4b87df0bdc85235de1edab31bfcb0b6875cc3b30
Thazin <thazinnaingmm@gmail.com>
2023-10-18 10:51:23 +0630

    authProvider setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthProvider from './Provider/AuthProvider';
+
+const App = () => {
+  return (
+    <>
+    <AuthProvider/>
+    </>
+  );
+}
+
+export default App;
