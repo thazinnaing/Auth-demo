d8c4fea41ea17ff1b0c7b23cbe73409ff43f5a82
Thazin <thazinnaingmm@gmail.com>
2023-10-24 11:09:11 +0630

    Registration Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthProvider from './Provider/AuthProvider';
 import Routes from './routes/Routes';
+import "./App.css"
 
 const App = () => {
   return (
---
+++ src/Pages/Register.jsx
@@ -0,0 +1,146 @@
+
+import { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const USER_REGEX= /^[A-Za-z][A-Za-z0-9\-_\s]{3,}$/;
+const PWD_REGEX= /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+
+const Register=()=>{
+    const navigate=useNavigate()
+    const userRef=useRef()
+    const errRef=useRef()
+
+    const [userName, setUserName]= useState("");
+    const [validName, setValidName]=useState(false);
+
+    const [password, setPassword]=useState("");
+    const [validPassword, setValidPassword]=useState(false);
+
+    const [confirmPassword, setConfirmPassword]=useState("");
+    const [validConfirmPassword, setValidConfirmPassword]=useState(false);
+
+    const [matchPassword, setMatchPassword]=useState(false)
+
+    const [success, setSuccess]=useState(false);
+    const [submit, setSubmit]=useState(false);
+
+    const canSubmit=[userName, password, confirmPassword].every(Boolean);
+    const handleSubmit=[validName, matchPassword].every(Boolean)
+
+
+    // console.log("userName", userName)
+    // console.log("password", password)
+    // console.log("confirmpassword", confirmPassword)
+    // console.log("match pass", matchPassword)
+    // console.log("cansubmit", canSubmit)
+    // console.log("handleSubmit",handleSubmit)
+    // console.log("success", success)
+    // console.log("submit", submit)
+
+    useEffect(()=>{
+        userRef.current.focus();
+    },[])
+
+    useEffect(()=>{
+        const result=USER_REGEX.test(userName);
+        setValidName(result)
+
+    }, [userName])
+
+    useEffect(()=>{
+        const result=PWD_REGEX.test(password);
+        setValidPassword(result)
+
+    }, [password])
+
+    useEffect(()=>{
+        const result=PWD_REGEX.test(confirmPassword);
+        setValidConfirmPassword(result)
+
+        if(password.length > 0){
+            setMatchPassword(password === confirmPassword)
+        }
+
+    }, [confirmPassword, password])
+
+
+    const onClickHandler=(e)=>{
+        e.preventDefault();
+        setSubmit(x=> !x)
+        setSuccess(handleSubmit)
+    }
+
+    
+    
+    return(
+        <section className="register-container">
+            <h2>Register Now!</h2>
+            {
+               submit && ( success ? <h2 className="success">Registration Successful  <span className="check"><FontAwesomeIcon icon={faCheck} /></span></h2> : <h3 className="unsuccess">Registration Unsuccessful : <span className="cross"><FontAwesomeIcon icon={faXmark} /></span></h3>)
+            }
+            <form className="form-container" autoComplete="off">
+                <label htmlFor="userName">UserName: </label>
+                <input 
+                    type="text"
+                    id="userName"
+                    ref={userRef}
+                    value={userName}
+                    onChange={(e)=> setUserName(e.target.value)}
+                />
+                {
+                    userName && <p className={validName ? "non-block" : "invalid"}>! invalid username.<br/> You can enter 4 to 24 characters including small letter and capital letters.</p>
+                }
+                
+                <label htmlFor="password">Password: </label>
+                <input 
+                    type="password"
+                    id="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value)}
+                />
+                {
+                    password && <p className={validPassword ? "non-block" : "invalid"}>! invalid password.<br/> You can enter from 8 characters including at least small letter, capital letter, number and special character like !@#$%.</p>
+                }
+
+                <label htmlFor="confirmPassword">Confirm Password: </label>
+                <input 
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e)=> setConfirmPassword(e.target.value)}
+                />
+                {
+                    confirmPassword && <p className={validConfirmPassword ? "non-block" : "invalid"}>! invalid confirmPassword.<br/> You can enter from 8 characters including at least small letter, capital letter, number and special character like !@#$%.</p>
+                }
+
+                {
+                    submit && (!matchPassword && <h3 className="unsuccess">Passwords are not same!</h3>)
+                }
+
+                <div className="btn-container">
+                <button className="register-btn"
+                    type="submit"
+                    disabled={!canSubmit}
+                    onClick={onClickHandler}
+                > Sign in
+                </button>
+
+                <button className="login-btn"
+                    type="button"
+                    onClick={()=>navigate("/login")}
+                > Login
+                </button>
+
+                </div>
+                
+            </form>
+    
+        </section>
+    )
+}
+
+export default Register;
