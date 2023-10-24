141051235df1bf92eef591d9425a88e2a1d8708d
Thazin <thazinnaingmm@gmail.com>
2023-10-24 11:49:02 +0630

    login form simple

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,25 +1,76 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../Provider/AuthProvider";
 import React from 'react';
+import { useRef, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from "../Provider/AuthProvider";
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Login = () => {
-    const {setToken}=useAuth();
-    const navigate=useNavigate();
 
-    const handleLogin=()=>{
-        setToken("Test token");
-        navigate("/", {replace: true})
-    };
+  const userRef=useRef();
+  const navigate=useNavigate();
+  const {setToken}=useAuth();
 
-    setTimeout(()=>{
-        handleLogin()
-    }, 2000);
+  const [userName, setUserName]= useState("");
+  const [password, setPassword]=useState("");
+  const [submit, setSubmit]=useState(false);
 
-  return (
-    <div>
-      Login Page
-    </div>
-  );
+  const validLogin=[userName,password].every(Boolean);
+
+  useEffect(()=>{
+    userRef.current.focus();
+  },[])
+
+  const onClickHandler=(e)=>{
+    e.preventDefault();
+    setToken("Test token");
+    setSubmit(x=> !x)
+    navigate("/")
+    
+  }
+
+  return(
+    <section className="login-container">
+        <h2>Login Now!</h2>
+
+        <form className="form-container" autoComplete="off">
+            <label htmlFor="userName">UserName: </label>
+            <input 
+                type="text"
+                id="userName"
+                ref={userRef}
+                value={userName}
+                onChange={(e)=> setUserName(e.target.value)}
+            />
+            
+            <label htmlFor="password">Password: </label>
+            <input 
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}
+            />
+
+            <div className="btn-container">
+              <button className="register-btn"
+                  type="submit"
+                  onClick={onClickHandler}
+                  disabled={!validLogin}
+              > Login
+              </button>
+              
+              <button className="login-btn"
+                  type="button"
+                  onClick={()=>navigate("/register")}
+              > Sign in
+              </button>
+
+            </div>
+            
+        </form>
+
+    </section>
+  )
 }
 
 export default Login;
